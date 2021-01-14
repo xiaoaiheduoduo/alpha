90415d05420e0560bcc01f8b86fa277a631cc822
coderd <likesimplemail@gmail.com>
2021-01-14 17:49:02 +0800

    Updated: health endpoints

--- ginwrapper/ginwrapper.go
+++ ginwrapper/ginwrapper.go
@@ -7,7 +7,32 @@ import (
 	"github.com/alphaframework/alpha/alog"
 )
 
-func New() *gin.Engine {
+func defaultHealthHandler(c *gin.Context) {
+	c.String(200, "")
+}
+
+type Options struct {
+	// LivezHandler checks if the app is alive
+	LivezHandler func(c *gin.Context)
+	// ReadyzHandler checks if the app is ready for incoming requests
+	ReadyzHandler func(c *gin.Context)
+}
+
+func (o *Options) complete() {
+	if o.LivezHandler == nil {
+		o.LivezHandler = defaultHealthHandler
+	}
+	if o.ReadyzHandler == nil {
+		o.ReadyzHandler = defaultHealthHandler
+	}
+}
+
+func New(options *Options) *gin.Engine {
+	if options == nil {
+		options = &Options{}
+	}
+	options.complete()
+
 	r := gin.New()
 	r.Use(GinResponseBodyLogMiddleware())
 	r.Use(Tracer())
@@ -17,17 +42,12 @@ func New() *gin.Engine {
 	r.NoRoute(NoRoute())
 	r.NoMethod(NoMethod())
 
-	addHealthEndpoints(r)
+	addHealthEndpoints(r, options)
 
 	return r
 }
 
-func addHealthEndpoints(r *gin.Engine) {
-	// For health checking
-	r.GET("/livez", func(c *gin.Context) {
-		c.String(200, "")
-	})
-	r.GET("/readyz", func(c *gin.Context) {
-		c.String(200, "")
-	})
+func addHealthEndpoints(r *gin.Engine, options *Options) {
+	r.GET("/livez", options.LivezHandler)
+	r.GET("/readyz", options.ReadyzHandler)
 }
