80aab120858808be54d0a79795bd054b8f0e6def
hongbo <liuhongbo@outlook.com>
2021-12-29 14:34:12 +0800

    package version

---
+++ version/base.go
@@ -0,0 +1,8 @@
+package version
+
+var (
+	gitVersion string
+	gitCommit  string // sha1 from git, output of $(git rev-parse HEAD)
+
+	buildDate = "1970-01-01T00:00:00Z" // build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
+)
